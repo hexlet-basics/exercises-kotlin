ae8fea721ba623a4345ef019307c4cf68b55b030
Sergey Ridev <whereareyo@yahoo.com>
2021-12-07 16:43:29 +0300

    tests added

---
+++ modules/10-basics/40-testing/Test.kt
@@ -0,0 +1,34 @@
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.nio.charset.StandardCharsets
+import java.util.stream.Collectors
+
+fun main() {
+    val expected = 420262531
+
+    val p = Runtime.getRuntime().exec("kotlin -classpath Main.jar MainKt")
+    p.waitFor()
+    val input = BufferedReader(
+        InputStreamReader(p.inputStream, StandardCharsets.UTF_8)
+    )
+        .lines()
+        .collect(Collectors.joining("\n"))
+    val error = BufferedReader(
+        InputStreamReader(p.errorStream, StandardCharsets.UTF_8))
+        .lines()
+        .collect(Collectors.joining("\n"))
+
+    println(input)
+
+    if (!expected.equals(input) || error.isNotBlank()) {
+        println("Test(statements): ERROR")
+        throw Exception(
+            "Expected: "
+                    + expected
+                    + ", but got: "
+                    + input
+                    + "\n"
+                    + error
+        )
+    }
+}
