b5569bddb4fb7983687ea9b430fe3febe11d1d53
Stanislav Dzisiak <stanislav.dzisiak@outlook.com>
2022-01-01 18:27:33 +0200

    fix tests

--- bin/test.sh
+++ bin/test.sh
@@ -3,10 +3,11 @@
 set -e
 
 export EXERCISE=$(basename `pwd`)
-export BUILD_EXERCISES_PATH=$BUILD_PATH/$EXERCISE
+export BUILD_EXERCISE_PATH=$BUILD_PATH/$EXERCISE
 export JAVA_OPTS="-noverify"
-export COMPILER_OPTS="-nowarn -Xeffect-system -Xno-optimize -Xskip-runtime-version-check -no-jdk -no-reflect -no-stdlib -classpath $CLASSPATH -d $BUILD_EXERCISES_PATH"
+export COMPILER_OPTS="-nowarn -Xeffect-system -Xno-optimize -Xskip-runtime-version-check -no-jdk -no-reflect -no-stdlib -classpath $CLASSPATH -d $BUILD_EXERCISE_PATH"
 
+rm -f $BUILD_EXERCISE_PATH/MainKt.class
 java $JAVA_OPTS org.jetbrains.kotlin.preloading.Preloader org.jetbrains.kotlin.cli.jvm.K2JVMCompiler $COMPILER_OPTS Main.kt 2>&1
-[ ! -z $COMPILE_TESTS ] && javac -cp $CLASSPATH:$BUILD_EXERCISES_PATH Test.java -d $BUILD_EXERCISES_PATH 2>&1
-java -cp $CLASSPATH:$BUILD_EXERCISES_PATH Test 2>&1
+[ ! -z $COMPILE_TESTS ] && javac -cp $CLASSPATH:$BUILD_EXERCISE_PATH Test.java -d $BUILD_EXERCISE_PATH 2>&1
+java -cp $CLASSPATH:$BUILD_EXERCISE_PATH Test 2>&1
